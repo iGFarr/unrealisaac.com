7fe70dda91808a414d26ebbc1c41ca8828a53aff
Isaac Farr <isaac.farr@snhu.edu>
2023-03-21 03:32:56 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function(){
+    $(".infinite-link").click(function(){
+        loadImage(0);loadImage(0);loadImage(0);loadImage(0);
+    });
+	
+	var isTouchDevice = 'ontouchstart' in document.documentElement;
+	if (isTouchDevice) {
+		$('#loading').hide();	
+	} else {
+		$('.wrap-infinite-link').hide();
+	}
+	
+});
+
+	
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+});
+ /*Scroll to top when arrow up clicked END*/
